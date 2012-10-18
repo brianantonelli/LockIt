4563e0dd00b8a612a3a3aaba8208d0ea1fff42cf
Brian Antonelli <brian.antonelli@gmail.com>
2012-10-17 21:04:40 -0400

    Adding MKNetwork framework

---
+++ osx/LockItBTServer/LockItBTServer/BTEngine.h
@@ -0,0 +1,13 @@
+//
+//  BTEngine.h
+//  LockItBTServer
+//
+//  Created by Brian Antonelli on 10/16/12.
+//  Copyright (c) 2012 Brian Antonelli. All rights reserved.
+//
+
+#import "MKNetworkEngine.h"
+
+@interface BTEngine : MKNetworkEngine
+
+@end
---
+++ osx/LockItBTServer/LockItBTServer/BTEngine.m
@@ -0,0 +1,13 @@
+//
+//  BTEngine.m
+//  LockItBTServer
+//
+//  Created by Brian Antonelli on 10/16/12.
+//  Copyright (c) 2012 Brian Antonelli. All rights reserved.
+//
+
+#import "BTEngine.h"
+
+@implementation BTEngine
+
+@end
