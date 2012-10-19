3410909efeced83a1e62fe5f406b3f758fa440f0
Brian Antonelli <brian.antonelli@gmail.com>
2012-10-19 14:29:58 -0300

    Cleaned up and added method for reading voltage.

--- arduino/lockit/lockit.ino
+++ arduino/lockit/lockit.ino
@@ -82,13 +82,13 @@ void loop(){
     else if(btByte == BT_IN_UNLOCK){
       unlockDoor();
     }
-    else if(btByte == BT_IN_SEND_STATE){}
-    
-    if(lastStateLocked){
-      Serial1.print(BT_STATE_LOCK);
-    }
-    else{
-      Serial1.print(BT_STATE_UNLOCK);
+    else if(btByte == BT_IN_SEND_STATE){
+      if(lastStateLocked){
+        Serial1.print(BT_STATE_LOCK);
+      }
+      else{
+        Serial1.print(BT_STATE_UNLOCK);
+      }
     }
   }
   
@@ -154,11 +154,6 @@ boolean isHardwareButtonPressed(){
   return buttonState == HIGH;
 }
 
-double getPSUVoltage(){
-  // TODO: return the voltage!
-  return 1.0;
-}
-
 void updateLEDs(){
   if(lastStateLocked){
     digitalWrite(ledLockedPin, HIGH);
@@ -178,10 +173,36 @@ void updateLEDs(){
 }
 
 boolean isLowBattery(){
-  int voltage = getPSUVoltage();
+  int voltage = readVcc();
   batteryLow = (voltage <= lowVoltageThreshold);
   
   return batteryLow;
 }
 
+long readVcc(){
+  // http://provideyourown.com/2012/secret-arduino-voltmeter-measure-battery-voltage/
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+  ADMUX = _BV(MUX5) | _BV(MUX0);
+#elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+  ADMUX = _BV(MUX3) | _BV(MUX2);
+#else
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#endif  
+ 
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+ 
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+ 
+  long result = (high<<8) | low;
+ 
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
 // End physical box stuff
